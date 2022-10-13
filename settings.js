a11394964c8ad4ac9aabcf5e81e9559275df5705
deeikg <115475928+deeikg@users.noreply.github.com>
2022-10-13 16:46:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['94763422146']
+global.premium = ['94763422146']
+global.ownernomer = '94763422146'
+global.ownername = 'ron♱Bot'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
